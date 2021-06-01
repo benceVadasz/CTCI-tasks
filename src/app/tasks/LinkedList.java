4dc5b6f1f76f4fa261ef74285a1284213069765a
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-06-01 12:16:24 +0200

    add solution to deleteMiddleNode, SumLists and partition from Linked list

--- src/app/tasks/LinkedList.java
+++ src/app/tasks/LinkedList.java
@@ -1,5 +1,8 @@
 package app.tasks;
 
+import jdk.dynalink.NamedOperation;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -43,6 +46,23 @@ public class LinkedList {
         return count;
     }
 
+    public int get(int index) {
+        if (0 > index) {
+            throw new IndexOutOfBoundsException();
+        }
+        int i = 0;
+        Node current = head;
+        while (current != null) {
+            if (i == index) {
+                return current.value;
+            }
+            current = current.next;
+            i++;
+        }
+
+        throw new IndexOutOfBoundsException();
+    }
+
 
     /**
      * this function removes duplicates from an unordered singly linked list
@@ -64,5 +84,69 @@ public class LinkedList {
             }
             ptr1 = ptr1.next;
         }
+    }
+
+    public void deleteMiddleNode() {
+        if (head == null)
+            return;
+        if (head.next == null) {
+            return;
+        }
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            prev = slow;
+        }
+        prev.next = slow.next;
+    }
+
+    public LinkedList partition(int value) {
+        LinkedList less = new LinkedList();
+        LinkedList equalOrMore = new LinkedList();
+
+        Node current = head;
+        while (current != null) {
+            if (current.value < value) {
+                less.add(current.value);
+            } else {
+                equalOrMore.add(current.value);
+            }
+            current = current.next;
+        }
+        Node equalOrMoreCurrent = equalOrMore.head;
+        while (equalOrMoreCurrent.next != null) {
+            less.add(equalOrMoreCurrent.value);
+            equalOrMoreCurrent = equalOrMoreCurrent.next;
+        }
+        less.add(equalOrMoreCurrent.value);
+        return less;
+    }
+
+    public int sumLists(LinkedList first, LinkedList second) {
+        Node dummy = new Node(0);
+        Node temp = dummy;
+        int carry = 0;
+        Node currF = first.head;
+        Node currS = second.head;
+        while (currF != null || currS != null || carry == 1) {
+            int sum = 0;
+            if (currF != null) {
+               sum += currF.value;
+               currF = currF.next;
+            }
+            if (currS != null) {
+               sum += currS.value;
+               currS = currS.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            temp.next = new Node(sum % 10);
+            temp = temp.next;
+        }
+        return dummy.next.value;
     }
 }
--- src/tests/LinkedListTest.java
+++ src/tests/LinkedListTest.java
@@ -34,4 +34,64 @@ class LinkedListTest {
         ll.removeDups();
         assertEquals(2, ll.size());
     }
+
+    @Test
+    @Order(3)
+    void deleteMiddleNode_linkedListWithOnlyHead_returnsEmptyList() {
+        ll.add(123);
+        ll.deleteMiddleNode();
+        assertEquals(0, ll.size());
+    }
+
+    @Test
+    @Order(4)
+    void deleteMiddleNode_afterFunctionLinkedListWithThreeNodes_returnsWithTwo() {
+        ll.add(7);
+        ll.add(123);
+        ll.add(77);
+        ll.deleteMiddleNode();
+        assertEquals(2, ll.size());
+    }
+
+    @Test
+    @Order(5)
+    void deleteMiddleNode_afterFunctionLinkedListWithFourNodes_returnsWithThree() {
+        ll.add(7);
+        ll.add(123);
+        ll.add(77);
+        ll.add(77);
+        ll.deleteMiddleNode();
+        assertEquals(3, ll.size());
+    }
+
+    @Test
+    @Order(6)
+    void partition_returnsLinkedListOrdered() {
+        ll.add(1);
+        ll.add(4);
+        ll.add(3);
+        ll.add(2);
+        ll.add(5);
+        ll.add(2);
+        ll = ll.partition(3);
+        assertEquals(2, ll.get(1));
+        assertEquals(2, ll.get(2));
+        assertEquals(4, ll.get(3));
+        assertEquals(3, ll.get(4));
+        assertEquals(5, ll.get(5));
+    }
+
+    @Test
+    @Order(7)
+    void sumLists_addsTwoNumbersRepresentedAsLinkedLists_returnsLinkedListOfSum() {
+        LinkedList llTwo = new LinkedList();
+        ll.add(7);
+        ll.add(1);
+        ll.add(6);
+        llTwo.add(5);
+        llTwo.add(9);
+        llTwo.add(2);
+
+        assertEquals(2, ll.sumLists(ll, llTwo));
+    }
 }
